561c0c31c826504e55f335dec2d3bd3e5c96e8d9
ITTI✅ <136064354+Ittiphon17@users.noreply.github.com>
2025-05-16 00:42:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
-from NewRequest import scrape_financials
+from RequestFinancial import scrape_financials
 from to_json import convert_csv_to_json
 import pandas as pd
 
 def get_stock_tickers_from_csv(filename='Stocks_Name.csv'):
     try:
-        df = pd.read_csv(filename)
+        df = pd.read_csv(filename, encoding="utf-8")
         return df['Stocks_Name'].dropna().astype(str).str.strip().str.upper().tolist()
     except Exception as e:
         print(f"❌ Failed to read {filename}: {e}")
         return []
 
-def main():
+def scrape_and_convert_data():
     tickers = get_stock_tickers_from_csv()
     if not tickers:
-        print("❌ No tickers found. Exiting...")
-        return
-
+        raise ValueError("No tickers found. Exiting...")
+    
     for ticker in tickers:
         print(f"\n--- Scraping Financial Data for {ticker} ---")
         scrape_financials(ticker)
-
-if __name__ == "__main__":
-    main()
+    
+    print("\n--- Converting CSV to JSON ---")
     convert_csv_to_json()
 
-# https://stockanalysis.com/quote/bkk/siri/financials/
-
-# pip freeze > requirements.txt
-
-# pip install -r requirements.txt
+if __name__ == "__main__":
+    scrape_and_convert_data()
 
 # uvicorn get_api:app --host 0.0.0.0 --port 8000 --reload
